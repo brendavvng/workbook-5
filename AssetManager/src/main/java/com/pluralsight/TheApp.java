e161f236d4dae277d322fc08ea352c5c4d9d5474
Brenda Vang <vangsbrenda@gmail.com>
2025-05-14 14:23:09 -0500

    displaying asset collection, adding GUI

--- AssetManager/src/main/java/com/pluralsight/TheApp.java
+++ AssetManager/src/main/java/com/pluralsight/TheApp.java
@@ -25,15 +25,15 @@ public class TheApp {
         House houseOne = new House(250000.00, "2025-01-05", "Cat Home",
                 "123 Paw Street", 2000, 2, 4000);
 
-        House houseTwo = new House(350000.00, "2025-10-05", "Cozy Home",
+        House houseTwo = new House(350000.00, "2025-10-05", "Beach Home",
                 "444 Cherry Street", 4000, 1, 6500);
 
         // creating vehicle objects
 
-        Vehicle vehicleOne = new Vehicle(30000, "2021-05-02", "Sedan",
+        Vehicle vehicleOne = new Vehicle(30000, "2021-05-02", "My Beater Car",
                 "Toyota Corolla", 45000, 2021);
 
-        Vehicle vehicleTwo = new Vehicle(45000.00, "2024-01-04", "Luxury Sedan",
+        Vehicle vehicleTwo = new Vehicle(45000.00, "2024-01-04", "My Luxury Car",
                 "Lexus IS 350 F Sport", 5800, 2024);
 
 
@@ -43,7 +43,16 @@ public class TheApp {
         assets.add(vehicleOne);
         assets.add(vehicleTwo);
 
-        
+        // for loop for asset collection
+        for(Asset asset : assets) {
+            System.out.println("╭────── · · ୨୧ · · ──────╮");
+            System.out.println("Description: " + asset.getDescription());
+            System.out.println("Date acquired: " + asset.getDateAcquired());
+            System.out.println("Cost: $" + asset.getOriginalCost());
+            System.out.println("Value: $" + asset.getValue());
+            System.out.println("╰────── · · ୨୧ · · ──────╯");
+            System.out.println("˚₊ ˚ ‧₊ .:･˚₊ ˚ ‧₊ .:･˚₊ *˚˚₊ ˚");
+        }
 
 
 
